16d04d12a99ecc5c6b8f0a46cb17ef39eb7e5bbb
Youngmin <ncs01045@gmail.com>
2024-11-05 19:13:28 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,75 @@
 import Image from "next/image";
-import styles from "./page.module.css";
-
+import defualt from "../image/image.png";
+import byeoundong from "../image/byeundong.png";
+import profile from "../image/profile.png";
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div>
+      <div className="Left">
+        <h1>윤태식</h1>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={profile}
+          alt="semple"
+          width={200}
+          height={200}
+          className="border-radius"
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className="Right-image">
+          <div className="text-left">
+            <li>태식</li>
+            <li>INTP</li>
+            <li>어저고 저저꼬 여고생</li>
+            <li>만화작가</li>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div className="Right">
+        <h1>연재작</h1>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <Image
+                    src={defualt}
+                    alt="semple"
+                    width={100}
+                    height={100}
+                    className="border-radius"
+                  />
+                </td>
+                <td>변동</td>
+              </tr>
+              <tr>
+                <td>
+                  <Image
+                    src={byeoundong}
+                    alt="변동일지"
+                    width={100}
+                    height={100}
+                    className="border-radius"
+                  />
+                </td>
+                <td>변동일지</td>
+              </tr>
+              <tr>
+                <td>
+                  <Image
+                    src={defualt}
+                    alt="semple"
+                    width={100}
+                    height={100}
+                    className="border-radius"
+                  />
+                </td>
+                <td width={300}>???</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
